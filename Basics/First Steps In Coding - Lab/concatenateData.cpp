56f3d4b42a10d4f624fb065e41538b55dbfd29db
mmilushev91 <milushev_1990@abv.bg>
2025-08-05 11:07:37 +0300

    Create concatenateData.cpp

---
+++ Basics/First Steps In Coding - Lab/concatenateData.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string firstName;
+    string lastName;
+    int age;
+    string town;
+
+    cin >> firstName;
+    cin >> lastName;
+    cin >> age;
+    cin >> town;
+
+    cout << "You are " << firstName << ' ' << lastName << ", a " << age << "-years old person from " << town << "." << endl;
+
+    return 0;
+
+}
