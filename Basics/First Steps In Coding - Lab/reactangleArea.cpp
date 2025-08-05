0ca1582c0c8e604a55f70b5523bcd60e354b2bd1
mmilushev91 <milushev_1990@abv.bg>
2025-08-05 10:22:58 +0300

    Create reactangleArea.cpp

---
+++ Basics/First Steps In Coding - Lab/reactangleArea.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    int b;
+
+    cin >> a;
+    cin >> b;
+
+    int area = a * b;
+
+    cout << area << endl;
+
+    return 0;
+
+}
