c8a6bb8e004024255364cf2c80b54cc2a44b7632
mmilushev91 <milushev_1990@abv.bg>
2025-08-05 10:51:36 +0300

    Create inchesToCentimeters.cpp

---
+++ Basics/First Steps In Coding - Lab/inchesToCentimeters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double inch = 2.54;
+  double inches;
+
+  cin >> inches;
+
+  double centimeters = inches * inch;
+
+  cout << centimeters;
+
+  return 0;
+
+}
