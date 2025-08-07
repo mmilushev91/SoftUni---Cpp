9374ca0937ca270d918aea10d378ebd6c650de4b
mmilushev91 <milushev_1990@abv.bg>
2025-08-07 10:14:29 +0300

    Create excellentResult.cpp

---
+++ Basics/Conditional Statements - Lab/excellentResult.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+    double grade;
+    cin >> grade;
+    
+    if (grade >= 5.5) {
+        cout << "Excellent!";
+    }
+    
+    return 0;
+}
