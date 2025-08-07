deb8674e7b3481d1ad34805014680897db09b942
mmilushev91 <milushev_1990@abv.bg>
2025-08-07 11:41:05 +0300

    Create speedInfo.cpp

---
+++ Basics/Conditional Statements - Lab/speedInfo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+int main()
+{
+    double speed;
+    cin >> speed;
+    
+    if (speed <= 10) {
+        cout << "slow";
+    } else if (speed <= 50) {
+        cout << "average";
+    } else if (speed <= 150) {
+        cout << "fast";
+    } else if (speed <= 1000) {
+        cout << "ultra fast";
+    } else {
+        cout << "extremely fast";
+    }
+    
+    return 0;
+}
