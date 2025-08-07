c90a32418af1e495c4c976b7d696c17b34ccaf93
mmilushev91 <milushev_1990@abv.bg>
2025-08-07 11:04:27 +0300

    Create evenOrOdd.cpp

---
+++ Basics/Conditional Statements - Lab/evenOrOdd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+
+int main() {
+    int num;
+    cin >> num;
+    
+    if (num % 2 == 0) {
+        cout << "even";
+    } else {
+        cout << "odd";
+    }
+    
+    return 0;
+}
